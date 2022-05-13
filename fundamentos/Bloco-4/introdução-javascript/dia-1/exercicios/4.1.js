b78d82d951c9afdc169e00d6c353985876b7d7ba
Andrios Correa <andrioscorrea170685@gmail.com>
2022-05-13 15:16:18 -0300

    adiciona exercicio 11

--- fundamentos/Bloco-4/introdução-javascript/dia-1/exercicios/4.1.js
+++ fundamentos/Bloco-4/introdução-javascript/dia-1/exercicios/4.1.js
@@ -199,11 +199,37 @@ Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?*/
 
 
 
+let inss;
+let imposto;
+let salarioBruto = 2500;
+if (salarioBruto <=1556.94){
+    inss = salarioBruto * 0.08;
+    }else if (salarioBruto <= 2594.92){
+        inss = salarioBruto * 0.09
+    } else if (salarioBruto <= 5189.82) {
+        inss = salarioBruto * 0.11;
+      } else {
+        inss = 570.88;
+      }
+      
+
+let salarioLiquido = salarioBruto - inss;
 
 
 
+if (salarioBruto <= 1903.98){
+    imposto = 0;
+}else if (salarioBruto >= 1903.99 && salarioBruto <= 2826.65 ){
+    imposto = (salarioLiquido * 0.075) - 142.80;
+}else if (salarioBruto >=2826.66 && salarioBruto <= 3751.05){
+    imposto = (salarioLiquido * 0.15 ) - 354.80;
+}else if (salarioBruto >=3751.06 && salarioBruto <= 4664.68){
+    imposto = (salarioLiquido * 0.225) - 636.13;
+}else {
+    imposto = (salarioLiquido * 0.275) - 869.36;
+}
 
-
+console.log("Sálario liquido:" + (salarioLiquido - imposto) )
 
 
 
