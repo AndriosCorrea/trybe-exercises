e725603cae0ae12117ca287e1eaadfdaa61782c1
Andrios Correa <andrioscorrea170685@gmail.com>
2022-05-16 15:32:34 -0300

    exercicio 4.2

--- fundamentos/bloco-4/intro-javascript/dia-2/4.2.js
+++ fundamentos/bloco-4/intro-javascript/dia-2/4.2.js
@@ -92,6 +92,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // console.log(resultado);
 
 // 9. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
-for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index] / 2);
-  };
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index] / 2);
+//   };
